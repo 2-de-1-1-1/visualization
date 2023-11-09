8e53d937f316aebd74c01c53b436d6f7bbcac129
yedajeong <ydj8952@khu.ac.kr>
2023-11-09 14:37:04 +0900

    노드 크기 및 색상 수정, 바 차트 형식 통일

--- job/job_freq_hist.py
+++ job/job_freq_hist.py
@@ -10,20 +10,49 @@ import json
 plt.rc('font', family='AppleGothic')
 
 # 샘플 데이터 load
-with open('job.json', 'r') as f:
+with open('job/job.json', 'r') as f:
     job_data = json.load(f)
 
-# 채용 공고 freq (수정)
+# 채용 공고 freq
 position_freq = {}
 
 for job in job_data:
     for pos in job.get('positions', []):
         position_freq[pos] = position_freq.get(pos, 0) + 1
 
-# plt.hist(freq, label='bins=8')
-plt.bar(position_freq.keys(), position_freq.values(), width=0.75)
-plt.xlabel('position')
-plt.ylabel('frequency')
-plt.xticks(np.arange(0, len(position_freq.keys())), labels=position_freq.keys(), fontsize=7, rotation=45)
-plt.legend()
-plt.show()
+
+positions = []
+for pos in position_freq.keys():
+    position = pos
+
+    idx = pos.find('/')
+    if idx != -1:
+        position = pos[:idx+1] + '\n' + pos[idx+1:]
+
+    idx = pos.find('(')
+    if idx != -1:
+        position = pos[:idx] + '\n' + pos[idx:]
+
+    idx = pos.find(' ')
+    if idx != -1:
+        position = pos[:idx+1] + '\n' + pos[idx+1:]
+
+    positions.append(position)
+
+print(positions)
+
+
+# positions = ['\n'.join(list(pos)) for pos in position_freq.keys()]
+
+plt.figure(figsize=(30, 15))
+colors = ['red' if f >= 50 else 'green' for f in position_freq.values()]
+
+plt.bar(positions, position_freq.values(), width=0.75, color=colors)
+
+plt.title('포지션 별 채용 공고', fontsize=30)
+plt.xlabel('position', fontsize=20)
+plt.ylabel('frequency', fontsize=20)
+plt.xticks(np.arange(0, len(position_freq.keys())), labels=positions, fontsize=13)
+
+# plt.show()
+plt.savefig('matplotlib_res/test.png') # 경로 변경
--- job/job_tech_graph.py
+++ job/job_tech_graph.py
@@ -5,6 +5,8 @@ from networkx.algorithms import bipartite
 from matplotlib import rc
 import matplotlib.pyplot as plt
 
+plt.rc('font', family='AppleGothic')
+
 # 1. data load
     # missing: sample 94개 중 position 89개, tech_stacks 75개
 with open('job/job.json', 'r') as f:
@@ -25,28 +27,23 @@ for job in job_data:
 # 3. Graph node, edge 지정
 Bipart = nx.Graph()
 
-# Bipart.add_nodes_from(position, bipartite=0) # 그룹 1: position
-filtered_position = ['시스템/네트워크', '프론트엔드']
-Bipart.add_nodes_from(filtered_position, bipartite=0) # 그룹 1_필터링: filtered_position (임의 필터링)
+position = ['시스템/네트워크', '서버/백엔드', '프론트엔드', '머신러닝']
+Bipart.add_nodes_from(position, bipartite=0) # 그룹 1: position
 Bipart.add_nodes_from(tech_stack, bipartite=1) # 그룹 2: tech_stack
 
 # edges = set([])
 edges = []
 for job in job_data:
     for pos in job.get('positions', []):
-        # for tech in job.get('tech_stacks', []):
-        #     edges.append((pos, tech))
-        if pos in filtered_position:
+        if pos in position:
             for tech in job.get('tech_stacks', []):
                 edges.append((pos, tech))
-                # edges.add((pos, tech))
 
 Bipart.add_edges_from(edges)
 
-print(len(edges))
 
 # 4-1. bipartite graph
-# pos = nx.bipartite_layout(Bipart, filtered_position)
+# pos = nx.bipartite_layout(Bipart, position)
 # nx.draw(Bipart, pos=pos, with_labels=True, font_family='AppleGothic', font_size=5)
 # plt.savefig('networkx_res/bipartite.png')
 # plt.show()
@@ -56,33 +53,52 @@ print(len(edges))
 proj = bipartite.projected_graph(Bipart, tech_stack)
 pos = nx.spring_layout(proj)
 
-high_degree = []
-low_degree = []
-degree_thresh = 50  # 차수 임계치_노드 크기 설정
+
+# 차수 thresh 설정
+degree = []
+
 for node, deg in nx.degree(proj):
-    if deg > degree_thresh:
-        high_degree.append(node)
-    elif deg > 0:
-        low_degree.append(node)
+    degree.append(deg)
 
-high_subnet = proj.subgraph(high_degree)
-low_subnet = proj.subgraph(low_degree)
+degree = sorted(list(set(degree)))
+degree_thresh = degree[2] # 0, min값인 노드는 제외
+max_degree = degree[-1]
+min_degree = degree[1]
 
-plt.rcParams['figure.figsize'] = (8, 6)
+# 차수 낮은 노드는 제외
+high_degree_node = []
 
-# 차수 높은 subnet
-nx.draw_networkx_nodes(high_subnet, pos=pos, node_size=500, node_color='pink') 
-nx.draw_networkx_labels(high_subnet, pos=pos, font_size=10, font_color='red')
+for node, deg in nx.degree(proj):
+    degree.append(deg)
+    if deg >= degree_thresh:
+        high_degree_node.append(node)
 
-# 차수 낮은 subnet
-nx.draw_networkx_nodes(low_subnet, pos=pos, node_size=300, node_color='grey')
-nx.draw_networkx_labels(low_subnet, pos=pos, font_size=7, font_color='black')
+        
+proj_subnet = proj.subgraph(high_degree_node)
 
-# 그래프 전체 edge
-nx.draw_networkx_edges(proj, pos=pos, edge_color='lightgrey')
+# 노드:차수 pair 딕셔너리 생성
+node_degrees = dict(proj_subnet.degree())
+node_sizes = {node: degree * 20 for node, degree in node_degrees.items()} # 차수에 비례하는 크기 가짐
+node_values = {node: degree for node, degree in node_degrees.items()} # 차수에 따른 색상 지정
 
-# plt.savefig('networkx_res/projected.png')
-plt.show()
+
+cmap = plt.get_cmap('spring')
+plt.figure(figsize=(20, 15))
+
+# subnet node, label
+# nx.draw_networkx_nodes(proj_subnet, pos=pos, node_size=[node_sizes[n] for n in node_sizes.keys()], node_color=['pink' if node_sizes[n] > 50 * 100 else 'grey' for n in node_sizes.keys()]) 
+nx.draw_networkx_nodes(proj_subnet, pos=pos, node_size=[node_sizes[n] for n in node_sizes.keys()],
+                                            node_color=list(node_values.values()),
+                                            cmap=cmap,
+                                            vmin=min_degree, vmax=max_degree)
+nx.draw_networkx_labels(proj_subnet, pos=pos, font_size=17, font_color='black')
+
+# subnet edge
+nx.draw_networkx_edges(proj_subnet, pos=pos, edge_color='lightgrey')
+
+plt.title('포지션 별 연관 기술 스택', fontsize=30)
+plt.savefig('networkx_res/projected.png')
+# plt.show()
 
 
 # 4-3. tech_stack projection (가중치_ratio)
